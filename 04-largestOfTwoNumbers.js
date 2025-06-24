63e38e7d87d3f135ad5c15ef83ac490c6ecde247
Shubham Kumar <meshubham1995@gmail.com>
2025-06-24 21:53:56 +0530

    largest of two numbers

---
+++ 04-largestOfTwoNumbers.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+
+if(a > b){
+    console.log(`The largest number is: ${a}`);
+}else if(b > a){
+    console.log(`The largest number is: ${b}`);
+}else{
+    console.log("Both numbers are equal.");
+}
