71a3d4c38dd518c3a47393bd6154489875c6689f
Shubham Kumar <meshubham1995@gmail.com>
2025-06-29 09:26:32 +0530

    program to calculate square and cube of a number

---
+++ 12-FindTheSquareAndCubeOfNumber.js
@@ -0,0 +1,8 @@
+//  Program : Find the square and cube of a number
+
+function squareAndCubeOfNumber (num)
+{
+    return `Square of ${num} is ${num*num} and Cube of ${num} is ${num * num *num}`;
+}
+
+console.log(squareAndCubeOfNumber (10));
